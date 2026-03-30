1ef14038e08a045281c250b78d948e5ff1af3e72
leyen <672228275@qq.com>
2026-03-30 17:31:33 +0800

    init theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
+    <ThemeProvider enableSystem={true} attribute="class" defaultTheme="dark">
       {children}
     </ThemeProvider>
   );
